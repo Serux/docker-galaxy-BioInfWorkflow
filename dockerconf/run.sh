34fee8974a4f973036f290d1b88e177cfaa96466
Sergio Chico <serchi93@hotmail.com>
2018-06-03 03:44:11 +0200

    Create run.sh
    run container script

---
+++ dockerconf/run.sh
@@ -0,0 +1,8 @@
+#!\bin\bash
+# Starts container if exists, else creates it.
+sudo docker start bioinfworkflow
+if [ $? -eq 0 ]; then
+    echo OK
+else
+    sudo docker tun -d -p 8080:80 -v ~/galaxy_storage/:/export/ --name bioinfworkflow serux/docker-galaxy-bioinfworkflow  
+fi
