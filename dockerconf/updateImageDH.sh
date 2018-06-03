74e5c8ed8953710586151928fee611698e7b62cf
Sergio Chico <serchi93@hotmail.com>
2018-06-03 02:33:02 +0200

    Update updateImageDH.sh
    added build image

--- dockerconf/updateImageDH.sh
+++ dockerconf/updateImageDH.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Pulls the last image from the dockerhub repository serux/docker-galaxy-bioinfworkflow
+# Pulls and builds the last image from the dockerhub repository serux/docker-galaxy-bioinfworkflow
 # tag: latest
 sudo docker pull serux/docker-galaxy-bioinfworkflow
+sudo docker build serux/docker-galaxy-bioinfowkflow
