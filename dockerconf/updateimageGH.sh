5c79c1365c77fa38e45f68b37206858dec4487fb
Sergio Chico <serchi93@hotmail.com>
2018-06-03 02:28:18 +0200

    Create updateimageGH.sh
    Script that Removes repository if exists, clones the last Github Repository and builds the image

---
+++ dockerconf/updateimageGH.sh
@@ -0,0 +1,7 @@
+#!\bin\bash
+# Removes repository if exists, clones the last Github Repository and builds the image
+if [ -d "~/gitBioInfWorkflow" ]; then
+  rm -rf ~/gitBioInfWorkflow
+fi
+sudo git clone --recursive https://github.com/Serux/docker-galaxy-BioInfWorkflow.git ~/gitBioInfWorkflow
+docker build ~/gitBioInfWorkflow/galaxy/ -t serux/docker-galaxy-bioinfworkflow
