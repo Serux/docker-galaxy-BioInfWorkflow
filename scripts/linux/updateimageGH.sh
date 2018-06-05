3b9860b65ff1a2208edefd775bc5c331a32abef9
Sergio Chico <serchi93@hotmail.com>
2018-06-05 09:42:31 +0200

    Update updateimageGH.sh
    fix sudo

--- scripts/linux/updateimageGH.sh
+++ scripts/linux/updateimageGH.sh
@@ -4,4 +4,4 @@ if [ -d "~/gitBioInfWorkflow" ]; then
   rm -rf ~/gitBioInfWorkflow
 fi
 sudo git clone --recursive https://github.com/Serux/docker-galaxy-BioInfWorkflow.git ~/gitBioInfWorkflow
-docker build ~/gitBioInfWorkflow/galaxy/ -t serux/docker-galaxy-bioinfworkflow
+sudo docker build ~/gitBioInfWorkflow/galaxy/ -t serux/docker-galaxy-bioinfworkflow
