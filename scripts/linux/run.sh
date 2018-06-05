42dc046099b1b61acb0acf1e0561825a01501adc
Sergio Chico <serchi93@hotmail.com>
2018-06-05 05:05:34 +0200

    Update run.sh
    fix typo

--- scripts/linux/run.sh
+++ scripts/linux/run.sh
@@ -4,5 +4,5 @@ sudo docker start bioinfworkflow
 if [ $? -eq 0 ]; then
     echo OK
 else
-    sudo docker tun -d -p 8080:80 -v ~/galaxy_storage/:/export/ --name bioinfworkflow serux/docker-galaxy-bioinfworkflow  
+    sudo docker run -d -p 8080:80 -v ~/galaxy_storage/:/export/ --name bioinfworkflow serux/docker-galaxy-bioinfworkflow  
 fi
